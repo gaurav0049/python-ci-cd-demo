971833ad80f5fc702e3449a8fa55c8002f8b1faa
Bhattacharya <gaurav.bhattacharya@tektronix.com>
2026-02-03 16:33:48 +0530

    parametrised test added

--- test_app.py
+++ test_app.py
@@ -1,4 +1,10 @@
+import pytest
 from app import add
 
-def test_add():
+@pytest.mark.smoke
+def test_add_smoke():
     assert add(2, 3) == 5
+
+@pytest.mark.regression
+def test_add_regression():
+    assert add(5, 5) == 10
